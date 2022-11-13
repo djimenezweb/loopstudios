f8ecafd491954fb3fb5a76e430986b8b78c51b6e
djimenezchocron <117752348+djimenezchocron@users.noreply.github.com>
2022-11-13 17:18:56 +0100

    Práctica 8 terminada

---
+++ dist/js/scripts.js
@@ -0,0 +1,20 @@
+const menuIconElement = document.getElementById('menu-icon');
+const menuElement = document.getElementById('menu');
+const logoElement = document.getElementById('logo');
+
+menuIconElement.addEventListener('click', (event) => {
+
+    if (event.target.dataset.icon === 'hamburger') {
+        menuIconElement.src = './assets/images/icon-close.svg';
+        menuIconElement.dataset.icon = 'close';
+        menuElement.classList.add('menu--show');
+        logoElement.classList.add('logo--menu-show');
+    }
+    else {
+        menuIconElement.src = './assets/images/icon-hamburger.svg';
+        menuIconElement.dataset.icon = 'hamburger';
+        menuElement.classList.remove('menu--show');
+        logoElement.classList.remove('logo--menu-show');
+    }
+}
+)
